9ce3fa27b16f85e11c5b5c39117e54d2f8413fdc
adrsolheim <adrsolheim2@gmail.com>
2021-07-27 18:01:50 +0200

    Change fields to accept null values

--- brew_app/models.py
+++ brew_app/models.py
@@ -7,9 +7,15 @@ class Batch(models.Model):
     style = models.CharField(max_length=30)
     volume = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(100)])
     start_date = models.DateField('brewed', auto_now_add=True)
-    end_date = models.DateField('finished', blank=True)
-    score = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(100)], blank=True)
-    comment = models.CharField(max_length=255, blank=True)
+    end_date = models.DateField('finished')
+    score = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(100)], null=True, blank=True)
+    comment = models.CharField(max_length=255, null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+
     class Meta:
         # a list of batches will return the newest first
         ordering = ['-start_date']
+
+    
